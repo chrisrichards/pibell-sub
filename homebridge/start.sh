664b0c40d30f043ce2258d27255968aa57bfb5d9
Chris Richards <chris.richards@yellowfeather.co.uk>
2019-08-07 19:54:40 +0100

    Update

--- homebridge/start.sh
+++ homebridge/start.sh
@@ -6,5 +6,8 @@ mkdir -p /data/homebridge/
 echo Copying files
 cp /root/.homebridge/config.json /data/homebridge/
 
+echo avahi-browse
+avahi-browse -a
+
 echo Starting homebridge
 homebridge --user-storage-path /data/homebridge/
